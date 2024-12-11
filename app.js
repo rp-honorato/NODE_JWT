6402104d9dedcb964b4d3b1c534fe92106f9a6aa
rp-honorato <rpedro.honorato@gmail.com>
2024-12-11 16:54:12 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+/* imports */
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt= require('bcrypt')
+const jwt= require('jsonwebtoken')
